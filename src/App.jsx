414f318fbe7823483050b2f568659be8a36e2ca4
ajayananda <ajayananda56@gmail.com>
2024-05-24 21:38:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useReducer } from 'react'
+import './App.css'
+//useReducer
+//useReducer()
+// return [state, dispatch]
+
+// as an argument
+
+// 1. reducer funtion
+//2. initialState
+
+// dispatch function
+// we pass object(action) as an argument
+//reducer
+// state
+// action
+
+function reducer(state, action) {
+  console.log(state, action)
+  if (action.type === "INCREMENT") {
+    return { ...state, count: state.count + 1 }
+  }
+  if (action.type === "RESET") {
+    return { ...state, count: 0 }
+  }
+  if (action.type === "DECREMENT") {
+    return { ...state, count: state.count - 1 }
+  }
+}
+const initialState = {count: 0, firstName: "Harshit"}
+  function App() {
+//const [state, setState] = useState({count:0}) 
+const [state, dispatch] = useReducer(reducer, initialState)
+const handleIncrease =()=>{
+  dispatch({type: "INCREMENT"})
+};
+const handleReset=()=>{
+  dispatch({type: "RESET"})
+};
+const handleDecrease = ()=>{
+  dispatch({type: "DECREMENT"})
+}
+    return (
+      <>
+        <div className='App'>
+          <h1> useReducer Tutorial</h1>
+          <hr />
+          <h2>Hello, {state.firstName}</h2>
+          <h2>{state.count}</h2>
+          <button onClick={hanleIncrease} ></button>Increase<button button onClick={handleReset}>Reset</button><button onClick={handleDecrease}>Decrease</button>
+        </div>
+      </>
+    )
+  }
+
+export default App
